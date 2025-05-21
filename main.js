55a5fb8a79e5ff9f5286b5a467958e9c38d4bab0
hoquocnam92 <hoquocnam92@gmail.com>
2025-05-21 11:32:27 +0700

    alo xem ne

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 const slides = document.querySelector('.hero-section');
 let index = 0;
 
-setInterval(() => {
-    console.log("CHECK INDEX" , index)
-  index = (index + 1) % 3; // 3 là số lượng ảnh
-  slides.style.transform = `translateX(-${index * 100}%)`;
-}, 3000);
+ setInterval(() => {
+    index = (index + 1) % 3;
+    slides.style.transform = `translateX(${-index * 100}vw)`
+ }, 3000);
