e071eaeb5281988a5d535911ee5da7f7a1b36100
hoquocnam92 <hoquocnam92@gmail.com>
2025-05-21 11:18:52 +0700

    Da lam xong banner

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const slides = document.querySelector('.hero-section');
 let index = 0;
 
-// setInterval(() => {
-//     console.log("CHECK INDEX" , index)
-//   index = (index + 1) % 3; // 3 là số lượng ảnh
-//   slides.style.transform = `translateX(-${index * 100}%)`;
-// }, 3000);
+setInterval(() => {
+    console.log("CHECK INDEX" , index)
+  index = (index + 1) % 3; // 3 là số lượng ảnh
+  slides.style.transform = `translateX(-${index * 100}%)`;
+}, 3000);
