50b9668a4a7e5d1580befc258910c4ae8c93ca9e
hoquocnam92 <hoquocnam92@gmail.com>
2025-05-21 21:05:45 +0700

    fix before

--- main.js
+++
@@ -1,7 +0,0 @@
-const slides = document.querySelector('.hero-section');
-let index = 0;
-
- setInterval(() => {
-    index = (index + 1) % 3;
-    slides.style.transform = `translateX(${-index * 100}vw)`
- }, 3000);
