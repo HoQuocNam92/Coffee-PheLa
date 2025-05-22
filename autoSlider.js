d903125729f79c17e288caa3d6d26973de8d7cae
hoquocnam92 <hoquocnam92@gmail.com>
2025-05-22 14:45:20 +0700

    done details

--- autoSlider.js
+++
@@ -1,7 +0,0 @@
-
-let index = 0;
-let current = 0;
- setInterval(() => {
-    index = (index + 1) % 3;
-    slides.style.transform = `translateX(${-index * 100}vw)`
- }, 3000);
--- main.js
+++ main.js
@@ -1,32 +1,51 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const prevSlide = document.querySelector('.prev');
+  const nextSlide = document.querySelector('.next');
+  const list = document.querySelectorAll('.address');
+  let currentSlide = 0;
 
-const list = document.querySelectorAll('.address');
-const bars = document.querySelector('.bars');
-const close = document.querySelector('.close');
-const menu = document.querySelector('.menu');
-const mobile = document.querySelector('.mobiles');
-console.log("🚀 ~ mobile:", mobile)
+  function showSlider() {
+    list.forEach((value) => {
+      value.style.transform = `translateX(-${currentSlide * 100}%)`;
+    });
+  }
+  if(nextSlide ) {
+  nextSlide.addEventListener("click", () => {
+    if (currentSlide < list.length - 3) currentSlide++;
+    showSlider();
+  });
 
-bars.addEventListener("click",()=>{
-   close.classList.toggle("active");
-   menu.classList.toggle('active');
-   bars.style.display = 'none'
-   mobile.classList.add('mobileiframe')
-})
-close.addEventListener("click",()=>{
-   menu.classList.toggle('active');
-   bars.style.display = 'block'
-   close.classList.toggle("active");
-   mobile.classList.remove('mobileiframe')
+  }
 
+  if(prevSlide) {
+   prevSlide.addEventListener("click", () => {
+    if (currentSlide > 0) currentSlide--;
+    showSlider();
+  });
+  }
 
-   
-})
+  const slides = document.querySelector('.hero-section');
+  let heroIndex = 0;
+  setInterval(() => {
+    heroIndex = (heroIndex + 1) % 3;
+    slides.style.transform = `translateX(${-heroIndex * 100}vw)`;
+  }, 3000);
 
+  const bars = document.querySelector('.bars');
+  const close = document.querySelector('.close');
+  const menu = document.querySelector('.menu');
+  if(bars) {
+ bars.addEventListener("click", () => {
+    close.classList.toggle("active");
+    menu.classList.toggle('active');
+    bars.style.display = 'none';
+  });
 
-
-
-
-
-
-
-
+  close.addEventListener("click", () => {
+    menu.classList.toggle('active');
+    bars.style.display = 'block';
+    close.classList.toggle("active");
+  });
+  }
+ 
+});
